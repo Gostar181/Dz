adde2c593aa8aec4c9ab28fa50ffe264019c7ea5
gosta <gostar181@gmail.com>
2024-12-14 19:24:50 +0200

    Дз 3

---
+++ dz4.py
@@ -0,0 +1,19 @@
+class m:
+    def __init__(self):
+        self.name = "MMMMMMMMMMMMM"
+
+class n:
+    def __init__(self):
+        self.name = "NNNNNNNNNNNNNN"
+
+class b:
+    def __init__(self):
+        self.name = "BBBBBBBBBBBBBB"
+
+class mnb(m,n,b):
+    def __init__(self):
+        m.__init__(self)
+        n.__init__(self)
+        b.__init__(self)
+
+        print(self.name)
